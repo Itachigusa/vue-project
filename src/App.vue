0dcce53755d42e7c3f368d716e7ff05a9f2abf63
Itachigusa <37017098+Itachigusa@users.noreply.github.com>
2023-07-25 23:29:27 +0900

    day2

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,27 @@ export default{
     return {
       hoge:"igyo",
       isShow:true,
-      arr:["s","sa",5,7],
+      listArr:[0, 1, 3],
+      arr:[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610],
       car:{
         name: "kuruma",
         speed:"hayai",
         price:"takai"
-      }
+      },
+      counter:0
+    }
+  },
+  methods:{
+    toggleShow(){
+      this.isShow = !this.isShow;
+      console.log(this.isShow);
+    },
+    reset(){
+      this.counter = 0;
+    },
+    incrementX(add){
+      this.counter = this.counter + add;
+      console.log(this.counter);
     }
   }
 }
@@ -24,13 +39,19 @@ export default{
 
 <template>
   <header>
-    <button @click="isShow = !isShow"> toggle </button>
+    <p>{{counter}}</p>
+    <button @click="reset"> reset </button>
+    <button @click="incrementX(50)"> +50 </button>
+    <button @click="toggleShow"> toggle </button>
     <img v-if="isShow" alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <button v-for="item in arr" @click="incrementX(item)">{{item}}</button>
+
     <div class="wrapper">
-      <HelloWorld v-for="item in arr" :msg="item" />
+      <HelloWorld v-for="item in listArr" :msg="item" />
     </div>
     <ul>
-      <li v-for="item in arr">
+      <li v-for="item in listArr">
         {{item}}
       </li>
     </ul>
